24405e771bf4f059e68c1a9a035508b77695aaea
Michael Haas <haas@computerlinguist.org>
2016-10-30 16:12:07 +0100

    WIP: publish measurement via mqtt

--- boiler_watch/boiler_watch.ino
+++ boiler_watch/boiler_watch.ino
@@ -1,10 +1,34 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_101MS, TCS34725_GAIN_1X );
 
+#define MQTT_HOST "iot.eclipse.org"
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient, MQTT_HOST);
+
+
 enum indicator_colors_t {GREEN, AMBER, RED, DARK, UNKNOWN};
+
+const indicator_colors_t COLORS[] {GREEN, AMBER, RED, DARK, UNKNOWN};
+
+const int measurement_interval_ms = 10 * 1000;
+
+const int sampling_interval_ms = 101;
+const int samples_per_measurement = 10;
+const int measurement_duration = samples_per_measurement * sampling_interval_ms;
+
+
+
+int color_frequency_per_measurement[5];
+
+int lastSample = 0;
+int numberOfSamples = 0;
 
 struct light_sensor_reading_t  {
   uint16_t r;
@@ -14,9 +38,39 @@ struct light_sensor_reading_t  {
 
 };
 
+struct measurement_interpretation_t {
+  bool is_valid;
+  int dark_duty_cycle;
+  indicator_colors_t other_color;
+};
+
+void connectToWifi() {
+
+
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(10);
+    WiFi.mode(WIFI_STA);
+    // ****************
+    WiFi.begin("FOO", "BAR");
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(200);
+      delay(50);
+
+    }
+  }
+  
+  if (client.connect("boiler-watch")) {
+    Serial.println("Connected to MQTT server");
+  } else {
+    Serial.println("Could not connect to MQTT server!");
+  }
+
+}
+
 void setup(void) {
   Serial.begin(9600);
-
+  connectToWifi();
   if (tcs.begin()) {
     Serial.println("Found sensor");
   } else {
@@ -27,11 +81,90 @@ void setup(void) {
   // Now we're ready to get readings!
 }
 
+bool canBeginMeasurement() {
+  if (lastSample + measurement_interval_ms <= millis()) {
+    return true;
+  }
+  return false;
+}
 
+void beginMeasurement() {
+  numberOfSamples = 0;
+  color_frequency_per_measurement[GREEN] = 0;
+  color_frequency_per_measurement[AMBER] = 0;
+  color_frequency_per_measurement[RED] = 0;
+  color_frequency_per_measurement[DARK] = 0;
+  color_frequency_per_measurement[UNKNOWN] = 0;
+
+}
+
+bool isMeasurementDone() {
+  if (numberOfSamples == samples_per_measurement) {
+    return true;
+  }
+  return false;
+}
+
+bool isMeasurementInProgress()  {
+  return numberOfSamples < samples_per_measurement;
+}
+
+bool canRecordNewSample() {
+  if (lastSample + sampling_interval_ms <= millis()) {
+    return true;
+  }
+  return false;
+}
+
+
+void recordSample() {
+  indicator_colors_t color = readIndicatorColor();
+  color_frequency_per_measurement[color]++;
+  lastSample = millis();
+  numberOfSamples++;
+}
+
+measurement_interpretation_t interpretMeasurement()  {
+  // We care about one thing: the duty cycle of the LED. Per measurement, we allow
+  // only one color + DARK. Multiple colors are not allowed, only DARK is allowed.
+
+  int dark_frequency = color_frequency_per_measurement[DARK];
+  indicator_colors_t observed_color = UNKNOWN;
+
+  measurement_interpretation_t interpretation;
+
+
+  for (int i = 0; i < sizeof(COLORS) / sizeof(indicator_colors_t); i++) {
+    indicator_colors_t color = COLORS[i];
+    if (color == DARK) {
+      continue;
+    }
+    int color_frequency = color_frequency_per_measurement[color];
+    if (color_frequency > 0) {
+      observed_color = color;
+    }
+  }
+
+  // We only allow at most one color other than dark
+  // The check below even works if only dark was observed, since the frequency for UNKNOWN will then be 0
+  if (dark_frequency + color_frequency_per_measurement[observed_color] != samples_per_measurement) {
+    interpretation.is_valid = false;
+  } else if (color_frequency_per_measurement[UNKNOWN] > 0) {
+    interpretation.is_valid = false;
+  } else {
+    interpretation.is_valid = true;
+    interpretation.other_color = observed_color;
+    interpretation.dark_duty_cycle = (dark_frequency * 100) / samples_per_measurement;
+  }
+
+  return interpretation;
+
+
+}
 
 enum indicator_colors_t readIndicatorColor() {
   light_sensor_reading_t reading = readRawLightSensor();
-  printSensorReading(reading);
+  //printSensorReading(reading);
   indicator_colors_t value = convertSensorReadingToColor(reading);
   return value;
 }
@@ -44,7 +177,7 @@ enum indicator_colors_t convertSensorReadingToColor(light_sensor_reading_t readi
   // Else, either RED or GREEN wins.
   // If the clear value is below 100, we assume the LED is off and DARK.
 
-  // Here are the values taken from my smartphone display with a colorized screen and 
+  // Here are the values taken from my smartphone display with a colorized screen and
   // Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_101MS, TCS34725_GAIN_1X );
 
   // Red screen: R: 75 G: 18 B: 17 C: 105
@@ -54,11 +187,11 @@ enum indicator_colors_t convertSensorReadingToColor(light_sensor_reading_t readi
   // TODO: convert to relative delta
   const int amber_delta = 20;
   const int minimum_value = 50;
- 
+
   if (reading.c < 100) {
     return DARK;
   }
-  
+
   // If blue dominates, we do not want to match anything
   if (reading.b > reading.r && reading.b > reading.g) {
     return UNKNOWN;
@@ -100,7 +233,7 @@ String convertColorToString (indicator_colors_t color) {
   } else {
     return String("invalid color code provided!");
   }
-}  
+}
 
 light_sensor_reading_t readRawLightSensor() {
 
@@ -116,11 +249,48 @@ light_sensor_reading_t readRawLightSensor() {
   return reading;
 }
 
+void publishMeasurementInterpretation(measurement_interpretation_t interpretation) {
+
+
+
+  DynamicJsonBuffer  jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+
+  root["valid"] = interpretation.is_valid;
+
+  if (interpretation.is_valid) {
+    root["dark_duty_cycle"] = interpretation.dark_duty_cycle;
+    root["color"] = convertColorToString(interpretation.other_color);
+  }
+  
+  char json[1024];
+  root.printTo(json, 1024);
+
+  Serial.println("Publishing result!");
+  Serial.println(json);
+  client.publish("/reijoo2Ooquaibah/", json);
+
+}
 
 void loop(void) {
-  indicator_colors_t color = readIndicatorColor();
-  String display_value = convertColorToString(color);
-  Serial.println(display_value);
-  delay(500);
-  
+  if (! isMeasurementInProgress() && canBeginMeasurement()) {
+    Serial.println("Measurement not in progress. Beginning one!");
+    beginMeasurement();
+  }
+
+  if (isMeasurementInProgress()) {
+    if (canRecordNewSample()) {
+      Serial.println("Can record new sample. Recording one!");
+      recordSample();
+    }
+    if (isMeasurementDone()) {
+      Serial.println("Measurement done. Interpreting it!");
+      // TODO: Process and reset
+      measurement_interpretation_t interpretation = interpretMeasurement();
+      publishMeasurementInterpretation(interpretation);
+    }
+  }
+
+
 }
